99115a68bc74951d795f6909ed013dc977e2273c
GreamcAtch <123013184+GreamcAtch@users.noreply.github.com>
2023-10-22 16:01:29 +0200

    feat: add ExceptionHandler, add get endpoints

--- src/main/java/com/bankapp/app/controller/AgreementController.java
+++ src/main/java/com/bankapp/app/controller/AgreementController.java
@@ -4,10 +4,9 @@ package com.bankapp.app.controller;
 import com.bankapp.app.dto.AgreementDTO;
 import com.bankapp.app.service.AgreementService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,12 +16,29 @@ import java.util.List;
 public class AgreementController {
 
     private final AgreementService agreementService;
+
     @GetMapping("/all")
-    public List<AgreementDTO> findAll(){
+    public List<AgreementDTO> findAll() {
         return agreementService.findAll();
     }
+
     @GetMapping("/{id}")
-    public AgreementDTO getAgreementDTO (@PathVariable("id") String id){
+    public AgreementDTO getAgreementDTO(@PathVariable("id") String id) {
         return agreementService.getAgreementDTO(id);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<String> get(@PathVariable("id") String id) {
+
+        String uuidPattern = "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$";
+        if (!id.matches(uuidPattern)) {
+            throw new IllegalArgumentException("ID is not a valid UUID");
+        }
+        String response = "RESPONSE: ID is a valid UUID: " + id;
+        return ResponseEntity.ok(response);
+    }
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+        String errorMessage = "ERROR: " + ex.getMessage();
+        return ResponseEntity.status(HttpStatus.I_AM_A_TEAPOT).body(errorMessage);
     }
 }
--- src/main/java/com/bankapp/app/controller/TransactionController.java
+++ src/main/java/com/bankapp/app/controller/TransactionController.java
@@ -3,10 +3,9 @@ package com.bankapp.app.controller;
 import com.bankapp.app.dto.TransactionDTO;
 import com.bankapp.app.service.TransactionService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,13 +17,29 @@ public class TransactionController {
     private final TransactionService transactionService;
 
     @GetMapping("/all")
-    public List<TransactionDTO> findAll(){
+    public List<TransactionDTO> findAll() {
         return transactionService.findAll();
     }
+
     @GetMapping("/{id}")
-    public TransactionDTO getTransactionDTO(@PathVariable("id") String id){
+    public TransactionDTO getTransactionDTO(@PathVariable("id") String id) {
         return transactionService.getTransactionDTO(id);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<String> get(@PathVariable("id") String id) {
 
+        String uuidPattern = "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$";
+        if (!id.matches(uuidPattern)) {
+            throw new IllegalArgumentException("ID is not a valid UUID");
+        }
+        String response = "RESPONSE: ID is a valid UUID: " + id;
+        return ResponseEntity.ok(response);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+        String errorMessage = "ERROR: " + ex.getMessage();
+        return ResponseEntity.status(HttpStatus.I_AM_A_TEAPOT).body(errorMessage);
+    }
 }
