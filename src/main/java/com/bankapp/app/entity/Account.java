c83450eabacfa7869b9368c9f3314650d92af460
GreamcAtch <123013184+GreamcAtch@users.noreply.github.com>
2023-10-24 18:36:38 +0200

    Finish edits

--- src/main/java/com/bankapp/app/entity/Account.java
+++ src/main/java/com/bankapp/app/entity/Account.java
@@ -22,50 +22,100 @@ import java.util.UUID;
 @Getter
 @Setter
 @ToString
+/**
+ * Это сущность, которая представляет собой аккаунт (счет) в банковской системе.
+ * Эта сущность содержит информацию о конкретном счете и связанных с ним данных.
+ */
 public class Account {
 
+    /**
+     * Идентификации уникальной записи или объекта в базе данных
+     */
     @Id
     @GeneratedValue(generator = "UUID")
     @Column(name = "id")
     private UUID id;
 
+    /**
+     * Это название счета,
+     * которое помогает идентифицировать его и понять его предназначение.
+     */
     @Column(name = "name")
     private String accountName;
 
+
+    /**
+     * Тип счета, который может указывать на его характеристики,
+     * такие как "сберегательный счет", "текущий счет" и т. д.
+     */
     @Column(name = "account_type")
     @Enumerated(EnumType.STRING)
     private AccountType accountType;
 
+    /**
+     * Статус счета, который может указывать на его состояние,
+     * такие как "активен", "закрыт" и т. д.
+     */
     @Column(name = "status")
     @Enumerated(EnumType.STRING)
     private AccountStatus accountStatus;
 
+    /**
+     * Код валюты, в которой ведется учет средств на счете,
+     * например, "USD" или "EUR".
+     */
     @Column(name = "currency_code")
     @Enumerated(EnumType.STRING)
     private CurrencyCode currencyCode;
 
+    /**
+     * Баланс (остаток) средств на счете.
+     */
     @Column(name = "balance")
     private BigDecimal accountBalance;
 
+    /**
+     * Дата и время создания счета.
+     */
     @Column(name = "created_at")
     private Timestamp createdAt;
 
+    /**
+     * Дата и временя последнего обновления записи в базе данных.
+     */
     @Column(name = "updated_at")
     private Timestamp updatedAt;
 
+    /**
+     * Клиент, связанный с данным счетом.
+     */
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "client_id", referencedColumnName = "id")
     private Client client;
 
+    /**
+     * Список договоров или соглашений, связанных с этим счетом.
+     */
     @OneToMany(mappedBy = "account")
     private List<Agreement> agreementList;
 
+    /**
+     * Это операции списания средств с банковского счета или карточки
+     */
     @OneToMany(mappedBy = "transactionDebitAccount")
     private Set<Transaction> debitTransaction;
 
+    /**
+     * Это финансовая операция, при которой средства перечисляются
+     * на банковский счет или кредитную/дебетовую карту клиента
+     */
     @OneToMany(mappedBy = "transactionCreditAccount")
     private Set<Transaction> creditTransaction;
 
+    /**
+     * Представляет собой класс с различными свойствами,
+     * представляющими информацию о банковской карте.
+     */
     @OneToOne(mappedBy = "account", fetch = FetchType.EAGER)
     private Card accountCard;
 
--- src/main/java/com/bankapp/app/entity/Client.java
+++ src/main/java/com/bankapp/app/entity/Client.java
@@ -16,44 +16,98 @@ import java.util.UUID;
 @NoArgsConstructor
 @Getter
 @Setter
+/**
+ * Клиенты - это физические лица или юридические лица,
+ * которые имеют отношения с банком и пользуются его услугами.
+ * Клиенты могут выполнять различные операции и иметь разные виды банковских счетов
+ * и продуктов.
+ */
 public class Client {
 
+    /**
+     * Идентификации уникальной записи или объекта в базе данных
+     */
     @Id
     @GeneratedValue(generator = "UUID")
     @Column(name = "id")
     private UUID id;
 
+    /**
+     * Это атрибут, который обычно описывает статус или состояние клиента
+     * в банковском приложении или системе.
+     */
     @Column(name = "client_status")
     @Enumerated(EnumType.STRING)
     private ClientStatus clientStatus;
 
+    /**
+     * Это уникальный идентификационный номер,
+     * который присваивается физическим и юридическим лицам в целях налогового учета
+     */
     @Column(name = "tax_code")
     private String taxCode;
 
+    /**
+     * Поле, обозначающее имя клиента (физического лица) в банковском приложении
+     * или информационной системе банка.
+     */
     @Column(name = "first_name")
     private String clientFirstName;
 
+
+    /**
+     * Поле, обозначающее фамилию клиента (физического лица) в банковском приложении
+     * или информационной системе банка.
+     */
     @Column(name = "last_name")
     private String clientLastName;
 
+
+    /**
+     * Электронная почта часто связана с аккаунтом клиента и может использоваться
+     * для уведомлений, обмена информацией о транзакциях,
+     * активности счета и других операциях.
+     */
     @Column(name = "email")
     private String email;
 
+    /**
+     * Информация об адресе места проживания клиента
+     */
     @Column(name = "address")
     private String address;
 
+    /**
+     * Информация о номере телефона клиента может быть собрана для связи с клиентом,
+     * отправки SMS-уведомлений, аутентификации или как один из способов связи
+     * с клиентом.
+     */
     @Column(name = "phone_number")
     private String phoneNumber;
 
+    /**
+     * Дата и время создания клиента.
+     */
     @Column(name = "created_at")
     private Timestamp createdAt;
 
+    /**
+     * Дата и временя последнего обновления записи в базе данных.
+     */
     @Column(name = "updated_at")
     private Timestamp updatedAt;
 
+
+    /**
+     * Представляют собой счета клиентов, на которых хранятся и управляются денежными средствами.
+     */
     @OneToMany(mappedBy = "client")
     private List<Account> accounts;
 
+    /**
+     * Использоваться для обозначения сотрудника банка,
+     * который ответственен за управление и обслуживание клиентов.
+     */
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "manager_id", referencedColumnName = "id")
     private Manager manager;
