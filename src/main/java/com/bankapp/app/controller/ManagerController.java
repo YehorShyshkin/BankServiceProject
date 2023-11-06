c0f86087d702dd9c4b9b05ba2d1ea89e03406c56
GreamcAtch <123013184+GreamcAtch@users.noreply.github.com>
2023-11-06 21:57:30 +0100

    "Manager and Clint merge"

--- src/main/java/com/bankapp/app/controller/ManagerController.java
+++ src/main/java/com/bankapp/app/controller/ManagerController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 @RestController
@@ -33,29 +34,42 @@ public class ManagerController {
     }
 
     @PostMapping("/create_managers")
-    public ResponseEntity<String> createManager (@RequestBody Manager manager){
+    public ResponseEntity<String> createManager(@RequestBody Manager manager) {
         managerService.save(manager);
         return ResponseEntity.ok("Manager was create! Sucess!");
     }
+
     @PostMapping("/update_manager/{id}")
-    public ResponseEntity<Manager> updateManager(@PathVariable UUID id, @RequestBody ManagerDTO managerDTO){
+    public ResponseEntity<Manager> updateManager(@PathVariable UUID id, @RequestBody ManagerDTO managerDTO) {
         Manager updateManager = managerService.updateManager(id, managerDTO);
-        if (updateManager!=null){
+        if (updateManager != null) {
             return new ResponseEntity<>(updateManager, HttpStatus.ACCEPTED);
-        }else {
+        } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 
     @DeleteMapping("delete_manager/{id}")
-    public ResponseEntity<String> deleteManager (@PathVariable UUID id)
-    {
+    public ResponseEntity<String> deleteManager(@PathVariable UUID id) {
         boolean deleteManager = managerService.deleteManager(id);
-        if (deleteManager){
+        if (deleteManager) {
             return new ResponseEntity<>("Manager deleted successfully!", HttpStatus.ACCEPTED);
-        }
-        else {
+        } else {
             return new ResponseEntity<>("Manager not found!", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/manager_client/")
+    public ResponseEntity<String> mergeManagerAndClient(@RequestBody Map<String, UUID> request) {
+        UUID managerId = request.get("managerId");
+        UUID clientId = request.get("clientId");
+
+        boolean merge = managerService.mergeManagerAndClient(clientId, managerId);
+
+        if (merge) {
+            return new ResponseEntity<>("Manager has been successfully added to the client!", HttpStatus.ACCEPTED);
+        } else {
+            return new ResponseEntity<>("Manager has not been successfully added to the client!", HttpStatus.NOT_FOUND);
         }
     }
 
