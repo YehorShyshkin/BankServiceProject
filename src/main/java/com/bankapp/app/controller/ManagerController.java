ebf0cd886a6e5e825acd5209b936d0ef864d82b1
GreamcAtch <123013184+GreamcAtch@users.noreply.github.com>
2023-11-06 21:17:02 +0100

    "Manager was create part 2"

--- src/main/java/com/bankapp/app/controller/ManagerController.java
+++ src/main/java/com/bankapp/app/controller/ManagerController.java
@@ -4,10 +4,12 @@ import com.bankapp.app.dto.ManagerDTO;
 import com.bankapp.app.entity.Manager;
 import com.bankapp.app.service.ManagerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/managers")
@@ -34,6 +36,15 @@ public class ManagerController {
     public ResponseEntity<String> createManager (@RequestBody Manager manager){
         managerService.save(manager);
         return ResponseEntity.ok("Manager was create! Sucess!");
+    }
+    @PostMapping("/update_manager/{id}")
+    public ResponseEntity<Manager> updateManager(@PathVariable UUID id, @RequestBody ManagerDTO managerDTO){
+        Manager updateManager = managerService.updateManager(id, managerDTO);
+        if (updateManager!=null){
+            return new ResponseEntity<>(updateManager, HttpStatus.ACCEPTED);
+        }else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
 
--- src/main/java/com/bankapp/app/service/ManagerService.java
+++ src/main/java/com/bankapp/app/service/ManagerService.java
@@ -4,10 +4,17 @@ import com.bankapp.app.dto.ManagerDTO;
 import com.bankapp.app.entity.Manager;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface ManagerService {
     List<ManagerDTO> findAll();
+
     ManagerDTO getManagerDTO(String id);
+
     void save(Manager manager);
+
+    Manager findManagerById(UUID managerId);
+
+    Manager updateManager(UUID id, ManagerDTO managerDTO);
 
 }
