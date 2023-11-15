d34ffbbad603c7dcf947465ef84d763f76783c8f
GreamcAtch <123013184+GreamcAtch@users.noreply.github.com>
2023-11-15 16:54:58 +0100

    feat: add Manager creates Product

--- src/main/java/com/bankapp/app/service/ProductService.java
+++ src/main/java/com/bankapp/app/service/ProductService.java
@@ -8,9 +8,16 @@ import java.util.UUID;
 
 public interface ProductService {
     List<ProductDTO> findAll();
+
     ProductDTO getProductDTO(String id);
+
     Product findProductById(UUID productId);
+
     void save(Product product);
+
     boolean deleteProduct(UUID id);
+
     Product updateProduct(UUID id, ProductDTO productDTO);
+
+    boolean mergeProductAndManager(UUID managerId, UUID productId);
 }
