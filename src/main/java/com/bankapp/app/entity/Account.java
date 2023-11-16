4df55d00fe804c440022df2899906cf2e5ae2bf0
GreamcAtch <123013184+GreamcAtch@users.noreply.github.com>
2023-11-16 21:44:13 +0100

    "Create commit from all class"

--- src/main/java/com/bankapp/app/entity/Account.java
+++ src/main/java/com/bankapp/app/entity/Account.java
@@ -7,7 +7,6 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -23,16 +22,24 @@ import java.util.UUID;
 @NoArgsConstructor
 @Getter
 @Setter
-@ToString
 
 /**
+ * ----- Russian ------
  * Это сущность, которая представляет собой аккаунт (счет) в банковской системе.
  * Эта сущность содержит информацию о конкретном счете и связанных с ним данных.
+ *
+ * ----- English -------
+ * This entity represents an account in the banking system.
+ * It contains information about a specific account and related data.
  */
 public class Account {
 
     /**
-     * Идентификации уникальной записи или объекта в базе данных
+     * ----- Russian ------
+     * Идентификации уникальной записи или объекта в базе данных.
+     *
+     * ----- English -------
+     * Unique identifier of the record or object in the database.
      */
     @Id
     @GeneratedValue(generator = "UUID")
@@ -40,89 +47,140 @@ public class Account {
     private UUID id;
 
     /**
+     * ----- Russian ------
      * Это название счета,
      * которое помогает идентифицировать его и понять его предназначение.
+     *
+     * ----- English -------
+     * The name of the account, helping to identify it and understand its purpose.
      */
     @Column(name = "name")
     private String accountName;
 
 
     /**
+     * ---- Russian -------
      * Тип счета, который может указывать на его характеристики,
      * такие как "сберегательный счет", "текущий счет" и т. д.
+     *
+     * ----- English -------
+     * The type of the account, which may indicate its characteristics,
+     * such as "savings account," "checking account," etc.
      */
     @Column(name = "account_type")
     @Enumerated(EnumType.STRING)
     private AccountType accountType;
 
     /**
+     * ---- Russian -------
      * Статус счета, который может указывать на его состояние,
      * такие как "активен", "закрыт" и т. д.
+     *
+     * ----- English -------
+     * The status of the account, which may indicate its state,
+     * such as "active," "closed," etc.
      */
     @Column(name = "status")
     @Enumerated(EnumType.STRING)
     private AccountStatus accountStatus;
 
     /**
+     * ---- Russian -------
      * Код валюты, в которой ведется учет средств на счете,
      * например, "USD" или "EUR".
+     *
+     * ----- English -------
+     * The currency code in which funds in the account are tracked,
+     * e.g., "USD" or "EUR."
      */
     @Column(name = "currency_code")
     @Enumerated(EnumType.STRING)
     private CurrencyCode currencyCode;
 
     /**
+     * ---- Russian -------
      * Баланс (остаток) средств на счете.
+     *
+     * ----- English -------
+     * The balance (remaining funds) in the account.
      */
     @Column(name = "balance")
     private BigDecimal accountBalance;
 
     /**
+     * ---- Russian -------
      * Дата и время создания счета.
+     *
+     * ----- English -------
+     * Date and time of account creation.
      */
     @Column(name = "created_at", updatable = false)
     @CreationTimestamp
     private Timestamp createdAt;
 
     /**
+     * ---- Russian -------
      * Дата и временя последнего обновления записи в базе данных.
+     *
+     * ----- English -------
+     * Date and time of the last update to the database record.
      */
     @Column(name = "updated_at")
     @UpdateTimestamp
     private Timestamp updatedAt;
 
     /**
+     * ---- Russian -------
      * Клиент, связанный с данным счетом.
+     *
+     * ----- English -------
+     * The client associated with this account.
      */
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "client_id", referencedColumnName = "id")
     private Client client;
 
     /**
+     * ---- Russian -------
      * Список договоров или соглашений, связанных с этим счетом.
+     *
+     * ----- English -------
+     * The list of agreements associated with this account.
      */
     @OneToMany(mappedBy = "account", fetch = FetchType.EAGER)
     private List<Agreement> agreementList;
 
     /**
-     * Это операции списания средств с банковского счета или карточки
+     * ---- Russian -------
+     * Это операции списания средств с банковского счета или карточки.
+     *
+     * ----- English -------
+     * Operations representing withdrawal of funds from the bank account or card.
      */
     @OneToMany(mappedBy = "transactionDebitAccount")
     private Set<Transaction> debitTransaction;
 
     /**
+     * ---- Russian -------
      * Это финансовая операция, при которой средства перечисляются
-     * на банковский счет или кредитную/дебетовую карту клиента
+     * на банковский счет или кредитную/дебетовую карту клиента.
+     *
+     * ----- English -------
+     * Financial operation where funds are transferred
+     * to the client's bank account or credit/debit card.
      */
     @OneToMany(mappedBy = "transactionCreditAccount")
     private Set<Transaction> creditTransaction;
 
     /**
+     * ---- Russian -------
      * Представляет собой класс с различными свойствами,
      * представляющими информацию о банковской карте.
+     *
+     * ----- English -------
+     * Represents a class with various properties
+     * representing information about a bank card.
      */
-//    @OneToOne(mappedBy = "account", fetch = FetchType.EAGER)
-//    private Card accountCard;
 
     @OneToMany(mappedBy = "account", cascade = CascadeType.ALL)
     private List<Card> cards;
@@ -156,5 +214,20 @@ public class Account {
     @Override
     public int hashCode() {
         return Objects.hash(accountName, accountStatus);
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                ", accountName='" + accountName + '\'' +
+                ", accountType=" + accountType +
+                ", accountStatus=" + accountStatus +
+                ", currencyCode=" + currencyCode +
+                ", accountBalance=" + accountBalance +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                ", client=" + client +
+                '}';
     }
 }
