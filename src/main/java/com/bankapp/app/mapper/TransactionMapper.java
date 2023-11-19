a7e61463ebe6b98cf9ca33b15bdde4a7ef22e273
GreamcAtch <123013184+GreamcAtch@users.noreply.github.com>
2023-11-19 14:09:22 +0100

    feat: delete Transaction

--- src/main/java/com/bankapp/app/mapper/TransactionMapper.java
+++ src/main/java/com/bankapp/app/mapper/TransactionMapper.java
@@ -9,19 +9,8 @@ import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface TransactionMapper {
-//    @Mapping(source = "debitTransaction", target = "transactionDebitAccount.id")
-//    @Mapping(source = "creditTransaction", target = "transactionCreditAccount.id")
-//    @Mapping(source = "transactionDebitAccount.accountStatus", target = "debitAccountStatus")
-//    @Mapping(source = "transactionDebitAccount.currencyCode", target = "debitAccountCurrencyCode")
-//    @Mapping(source = "transactionDebitAccount.accountBalance", target = "debitAccountBalance")
-//
-//    @Mapping(source = "transactionCreditAccount.accountType", target = "creditAccountType")
-//    @Mapping(source = "transactionCreditAccount.accountStatus", target = "creditAccountStatus")
-//    @Mapping(source = "transactionCreditAccount.currencyCode", target = "creditAccountCurrencyCode")
-//    @Mapping(source = "transactionCreditAccount.accountBalance", target = "creditAccountBalance")
-
-
     TransactionDTO toDTO(Transaction transaction);
+
     List<TransactionDTO> toDTO(List<Transaction> transactionList);
 
     @Mapping(source = "debitAccountId", target = "transactionDebitAccount.id")
