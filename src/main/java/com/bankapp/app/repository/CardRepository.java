36003639970159fff9bb8c784508bbcf711ed285
GreamcAtch <123013184+GreamcAtch@users.noreply.github.com>
2023-12-13 18:44:06 +0100

    "Finish test Manager"

--- src/main/java/com/bankapp/app/repository/CardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bankapp.app.repository;
-
-import com.bankapp.app.entity.Card;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface CardRepository extends JpaRepository<Card, UUID> {
-}
